badfffa4b7d4c5ea09138455816fa0962fc511ee
Nikita <Pytnikrydakov@gmail.com>
2026-01-18 16:20:31 +0300

    Добавьте файлы проекта.

---
+++ стпо1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace стпо1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxH;
+        private System.Windows.Forms.TextBox textBoxEPSt;
+        private System.Windows.Forms.TextBox textBoxLog;
+
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button buttonSaveExcel;
+
+        private System.Windows.Forms.DataGridView dgv;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxH = new System.Windows.Forms.TextBox();
+            this.textBoxEPSt = new System.Windows.Forms.TextBox();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.buttonSaveExcel = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(46, 26);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(86, 20);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(46, 66);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(86, 20);
+            this.textBoxB.TabIndex = 1;
+            // 
+            // textBoxH
+            // 
+            this.textBoxH.Location = new System.Drawing.Point(46, 106);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(86, 20);
+            this.textBoxH.TabIndex = 2;
+            // 
+            // textBoxEPSt
+            // 
+            this.textBoxEPSt.Location = new System.Drawing.Point(46, 146);
+            this.textBoxEPSt.Name = "textBoxEPSt";
+            this.textBoxEPSt.Size = new System.Drawing.Size(86, 20);
+            this.textBoxEPSt.TabIndex = 3;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 270);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(120, 140);
+            this.textBoxLog.TabIndex = 4;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(12, 185);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(120, 30);
+            this.buttonCalc.TabIndex = 5;
+            this.buttonCalc.Text = "Рассчитать";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // buttonSaveExcel
+            // 
+            this.buttonSaveExcel.Location = new System.Drawing.Point(12, 225);
+            this.buttonSaveExcel.Name = "buttonSaveExcel";
+            this.buttonSaveExcel.Size = new System.Drawing.Size(120, 30);
+            this.buttonSaveExcel.TabIndex = 6;
+            this.buttonSaveExcel.Text = "Сохранить в Excel";
+            this.buttonSaveExcel.UseVisualStyleBackColor = true;
+            this.buttonSaveExcel.Click += new System.EventHandler(this.buttonSaveExcel_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.Location = new System.Drawing.Point(150, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 60;
+            this.dgv.Size = new System.Drawing.Size(450, 400);
+            this.dgv.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "b";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "h";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "EPSt";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(620, 430);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxH);
+            this.Controls.Add(this.textBoxEPSt);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.buttonSaveExcel);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Численное интегрирование";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ стпо1/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace стпо1
+{
+    public partial class Form1 : Form
+    {
+        bool errorExeCalled = false;// контроль однократного вызова
+        double currentEps;
+        string rawA;
+        string rawB;
+        string rawH;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitTable();
+        }
+
+        void InitTable()
+        {
+            dgv.RowCount = 20;
+            dgv.ColumnCount = 3;
+
+            dgv.Columns[0].HeaderText = "Парабола";
+            dgv.Columns[1].HeaderText = "Трапеции";
+            dgv.Columns[2].HeaderText = "Монте-Карло";
+
+            for (int i = 0; i < 20; i++)
+                dgv.Rows[i].HeaderCell.Value = $"{i + 1}";
+        }
+
+        // ======= ЭТАЛОН (Ньютон–Лейбниц) ==========
+        double ExactIntegral(double a, double b, int n)
+        {
+            double Fa = 0;
+            double Fb = 0;
+
+            for (int k = 0; k <= n; k++)
+            {
+                // первообразная: x^(k+1)
+                Fa += Math.Pow(a, k + 1);
+                Fb += Math.Pow(b, k + 1);
+            }
+
+            return Fb - Fa;
+        }
+
+        // ============ ВЫЗОВ Integral3x.exe ============
+        double CallIntegralExe(double a, double b, double h, int method, int degree)
+        {
+            string args = $"{a} {b} {h} {method}";
+            for (int i = 1; i <= degree + 1; i++)
+                args += $" {i}";
+
+            string output = CallIntegralExeRaw(args);
+
+            Match m = Regex.Match(output, @"S\s*=\s*([-+]?\d+(?:[.,]\d+)?)");
+            if (!m.Success)
+                throw new Exception("Integral3x.exe не вернул результат");
+
+            string number = m.Groups[1].Value.Replace(',', '.');
+            return double.Parse(number, CultureInfo.InvariantCulture);
+        }
+
+        string CallIntegralExeRaw(string args)
+        {
+            string exePath = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Integral3x.exe"
+            );
+
+            if (!System.IO.File.Exists(exePath))
+                throw new Exception("Integral3x.exe не найден");
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = args,
+
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                CreateNoWindow = true
+            };
+
+            using (Process p = Process.Start(psi))
+            {
+                // закрываем system("pause")
+                p.StandardInput.WriteLine();
+                p.StandardInput.Flush();
+
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+
+                return output;
+            }
+        }
+
+        // ================= КНОПКА =================
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+            errorExeCalled = false;
+            rawA = textBoxA.Text.Trim();
+            rawB = textBoxB.Text.Trim();
+            rawH = textBoxH.Text.Trim();
+
+            if (!double.TryParse(textBoxEPSt.Text, out double eps) || eps < 0)
+            {
+                MessageBox.Show(
+                    "Невозможно сохранить данные.\nПогрешность EPSt не задана или задана некорректно.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            currentEps = eps;
+
+
+            if (!TryReadInput(out double a, out double b, out double h, out string expectedError))
+            {
+                HandleInputError(a, b, h, 1, 1, expectedError);
+                return;
+            }
+
+            for (int n = 1; n <= 20; n++)
+                {
+                    double exact = ExactIntegral(a, b, n);
+
+                    for (int m = 1; m <= 3; m++)
+                    {
+                        try
+                        {
+                            double num = CallIntegralExe(a, b, h, m, n);
+                            double EPSf = Math.Abs(num - exact);
+
+                            var cell = dgv.Rows[n - 1].Cells[m - 1];
+                            cell.Value = EPSf.ToString();
+
+                            cell.Style.BackColor =
+                                EPSf <= eps ? Color.LightGreen : Color.LightCoral;
+                        }
+                        catch (Exception ex)
+                        {
+                            HandleInputError(a, b, h, m, n, ex.Message);
+                            return;
+                        }
+                    }
+                }
+        }
+
+        private void buttonSaveExcel_Click(object sender, EventArgs e)
+        {
+            if (currentEps <= 0)
+            {
+                MessageBox.Show(
+                    "Невозможно сохранить данные.\nПогрешность EPSt не задана или задана некорректно.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            if (!TryReadInput(out double a, out double b, out double h, out string expectedError))
+            {
+                MessageBox.Show(
+                    "Невозможно сохранить данные.\nПроверьте корректность исходных данных.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            SaveResultsToExcel(a, b, h, currentEps);
+        }
+
+        void HandleInputError(double a, double b, double h, int method, int degree, string expected)
+        {
+            if (errorExeCalled) return;
+            errorExeCalled = true;
+
+            List<string> argsList = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(rawA)) argsList.Add(rawA);
+            if (!string.IsNullOrWhiteSpace(rawB)) argsList.Add(rawB);
+            if (!string.IsNullOrWhiteSpace(rawH)) argsList.Add(rawH);
+            argsList.Add(method.ToString());
+            argsList.Add(degree.ToString());
+
+            string args = string.Join(" ", argsList);
+
+            string rawOutput = CallIntegralExeRaw(args);
+
+            string actual = rawOutput.TrimEnd();
+
+            // удаляем последнюю строку (pause)
+            int lastNewLine = actual.LastIndexOf('\n');
+            if (lastNewLine >= 0)
+                actual = actual.Substring(0, lastNewLine).TrimEnd();
+
+            textBoxLog.Text += "Негативный тест-кейс (ошибка ввода)\r\n\r\n" + "Ожидаемый отклик:\r\n" + expected + "\r\n\r\n" + "Фактический отклик:\r\n"
+                + actual;
+        }
+
+        void SaveResultsToExcel(double a, double b, double h, double eps)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Workbooks.Add();
+
+            Excel.Worksheet sheet = excel.ActiveSheet;
+            sheet.Name = "Результаты интегрирования";
+
+            int row = 1;
+
+            // ===== СЛУЖЕБНАЯ ИНФОРМАЦИЯ =====
+            sheet.Cells[row++, 1] = "Исходные данные";
+            sheet.Cells[row++, 1] = "Левая граница a:";
+            sheet.Cells[row - 1, 2] = a;
+
+            sheet.Cells[row++, 1] = "Правая граница b:";
+            sheet.Cells[row - 1, 2] = b;
+
+            sheet.Cells[row++, 1] = "Шаг h:";
+            sheet.Cells[row - 1, 2] = h;
+
+            sheet.Cells[row++, 1] = "Допустимая погрешность EPSt:";
+            sheet.Cells[row - 1, 2] = eps;
+
+            sheet.Cells[row++, 1] = "Эталонный метод:";
+            sheet.Cells[row - 1, 2] = "Ньютон–Лейбниц";
+
+            row += 2;
+
+            // ===== ЗАГОЛОВКИ ТАБЛИЦЫ =====
+            sheet.Cells[row, 1] = "Степень полинома";
+            sheet.Cells[row, 2] = "Парабола";
+            sheet.Cells[row, 3] = "Трапеции";
+            sheet.Cells[row, 4] = "Монте-Карло";
+
+            Excel.Range header = sheet.Range[
+                sheet.Cells[row, 1],
+                sheet.Cells[row, 4]
+            ];
+            header.Font.Bold = true;
+            header.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+            row++;
+
+            // ===== ДАННЫЕ ИЗ DataGridView =====
+            for (int i = 0; i < dgv.RowCount; i++)
+            {
+                sheet.Cells[row, 1] = i + 1;
+
+                for (int j = 0; j < dgv.ColumnCount; j++)
+                {
+                    var cell = dgv.Rows[i].Cells[j];
+                    sheet.Cells[row, j + 2] = cell.Value;
+
+                    // цвет ячейки
+                    if (cell.Style.BackColor == Color.LightGreen)
+                        sheet.Cells[row, j + 2].Interior.Color =
+                            ColorTranslator.ToOle(Color.LightGreen);
+
+                    if (cell.Style.BackColor == Color.LightCoral)
+                        sheet.Cells[row, j + 2].Interior.Color =
+                            ColorTranslator.ToOle(Color.LightCoral);
+                }
+                row++;
+            }
+
+            // автоширина
+            sheet.Columns.AutoFit();
+
+            excel.Visible = true;
+        }
+
+        // ============ ПРОВЕРКА ВВОДА ============
+        bool TryReadInput(out double a, out double b, out double h, out string expectedErrors)
+        {
+            expectedErrors = "";
+            bool hasError = false;
+            bool aFlag = false;
+            bool bFlag = false;
+
+            a = 0;
+            b = 0;
+            h = 0;
+
+            // ===== a =====
+            if (string.IsNullOrWhiteSpace(rawA))
+            {
+
+                expectedErrors = "Число параметров не соответствует ожидаемому и должно быть, как минимум 5!";
+                hasError = true;
+                return !hasError;
+
+            }
+            else if (!double.TryParse(rawA, out a))
+            {
+                expectedErrors += "Левая граница диапазона не является числом!\r\n";
+                hasError = true;
+            }
+
+            // ===== b =====
+            if (string.IsNullOrWhiteSpace(rawB))
+            {
+
+                expectedErrors = "Число параметров не соответствует ожидаемому и должно быть, как минимум 5!";
+                hasError = true;
+                return !hasError;
+
+            }
+            else if (!double.TryParse(rawB, out b))
+            {
+                expectedErrors += "Правая граница диапазона не является числом!\r\n";
+                hasError = true;
+            }
+
+            // ===== a < b =====
+            if (a >= b && !aFlag && !bFlag)
+            {
+                expectedErrors += "Левая граница диапазона должна быть < правой границы диапазона!\r\n";
+                hasError = true;
+            }
+
+            // ===== h =====
+            if (string.IsNullOrWhiteSpace(rawH))
+            {
+
+                expectedErrors = "Число параметров не соответствует ожидаемому и должно быть, как минимум 5!";
+                hasError = true;
+                return !hasError;
+
+            }
+            else if (!double.TryParse(rawH, out h))
+            {
+                h = 0;
+
+                if (h < 0.000001 || h > 0.5)
+                {
+                    expectedErrors += "Шаг интегрирования должен быть в пределах [0.000001;0.5]\r\n";
+                    hasError = true;
+                }
+
+            }
+
+            return !hasError;
+        }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
